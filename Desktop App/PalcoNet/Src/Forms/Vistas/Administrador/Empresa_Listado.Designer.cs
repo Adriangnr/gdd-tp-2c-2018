490d562e49ce32a4d0ef0aa941aa05f26c98f8f7
Matias <matias.ochoa19@gmail.com>
2018-12-01 19:35:03 -0300

    Resize empresa listados

--- Desktop App/PalcoNet/Src/Forms/Vistas/Administrador/Empresa_Listado.Designer.cs
+++ Desktop App/PalcoNet/Src/Forms/Vistas/Administrador/Empresa_Listado.Designer.cs
@@ -66,44 +66,44 @@
             // 
             // Entidad
             // 
-            this.Entidad.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.Entidad.Size = new System.Drawing.Size(122, 29);
+            this.Entidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Entidad.Size = new System.Drawing.Size(76, 18);
             this.Entidad.Text = "Empresas";
             // 
             // listBox1
             // 
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3);
-            this.listBox1.Size = new System.Drawing.Size(10, 4);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Size = new System.Drawing.Size(8, 4);
             // 
             // btn_cancel
             // 
-            this.btn_cancel.Location = new System.Drawing.Point(1446, 537);
-            this.btn_cancel.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btn_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_cancel.Location = new System.Drawing.Point(964, 349);
+            this.btn_cancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             // 
             // footer
             // 
-            this.footer.Location = new System.Drawing.Point(1282, 652);
-            this.footer.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.footer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.footer.Location = new System.Drawing.Point(855, 424);
+            this.footer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             // 
             // email
             // 
             this.email.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.email.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.email.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.email.Location = new System.Drawing.Point(28, 231);
-            this.email.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.email.Location = new System.Drawing.Point(19, 150);
             this.email.Name = "email";
-            this.email.Size = new System.Drawing.Size(216, 33);
+            this.email.Size = new System.Drawing.Size(145, 24);
             this.email.TabIndex = 79;
             // 
             // label_email
             // 
             this.label_email.AutoSize = true;
             this.label_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_email.Location = new System.Drawing.Point(28, 203);
-            this.label_email.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_email.Location = new System.Drawing.Point(19, 132);
             this.label_email.Name = "label_email";
-            this.label_email.Size = new System.Drawing.Size(54, 22);
+            this.label_email.Size = new System.Drawing.Size(39, 15);
             this.label_email.TabIndex = 75;
             this.label_email.Text = "Email";
             // 
@@ -111,10 +111,9 @@
             // 
             this.label_cuit.AutoSize = true;
             this.label_cuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_cuit.Location = new System.Drawing.Point(28, 120);
-            this.label_cuit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_cuit.Location = new System.Drawing.Point(19, 78);
             this.label_cuit.Name = "label_cuit";
-            this.label_cuit.Size = new System.Drawing.Size(52, 22);
+            this.label_cuit.Size = new System.Drawing.Size(34, 15);
             this.label_cuit.TabIndex = 78;
             this.label_cuit.Text = "CUIT";
             // 
@@ -123,10 +122,9 @@
             this.razon_social.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.razon_social.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.razon_social.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.razon_social.Location = new System.Drawing.Point(28, 68);
-            this.razon_social.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.razon_social.Location = new System.Drawing.Point(19, 44);
             this.razon_social.Name = "razon_social";
-            this.razon_social.Size = new System.Drawing.Size(216, 33);
+            this.razon_social.Size = new System.Drawing.Size(145, 24);
             this.razon_social.TabIndex = 73;
             // 
             // cuit
@@ -134,33 +132,32 @@
             this.cuit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.cuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cuit.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.cuit.Location = new System.Drawing.Point(28, 149);
-            this.cuit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cuit.Location = new System.Drawing.Point(19, 97);
             this.cuit.Name = "cuit";
-            this.cuit.Size = new System.Drawing.Size(216, 33);
+            this.cuit.Size = new System.Drawing.Size(145, 24);
             this.cuit.TabIndex = 77;
             // 
             // label_razon_social
             // 
             this.label_razon_social.AutoSize = true;
             this.label_razon_social.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_razon_social.Location = new System.Drawing.Point(28, 38);
-            this.label_razon_social.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_razon_social.Location = new System.Drawing.Point(19, 25);
             this.label_razon_social.Name = "label_razon_social";
-            this.label_razon_social.Size = new System.Drawing.Size(115, 22);
+            this.label_razon_social.Size = new System.Drawing.Size(80, 15);
             this.label_razon_social.TabIndex = 76;
             this.label_razon_social.Text = "Razon Social";
             // 
             // btn_habilitar
             // 
+            this.btn_habilitar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_habilitar.BackColor = System.Drawing.SystemColors.Highlight;
             this.btn_habilitar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_habilitar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_habilitar.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_habilitar.Location = new System.Drawing.Point(1446, 120);
+            this.btn_habilitar.Location = new System.Drawing.Point(964, 78);
             this.btn_habilitar.Margin = new System.Windows.Forms.Padding(0);
             this.btn_habilitar.Name = "btn_habilitar";
-            this.btn_habilitar.Size = new System.Drawing.Size(194, 69);
+            this.btn_habilitar.Size = new System.Drawing.Size(129, 45);
             this.btn_habilitar.TabIndex = 60;
             this.btn_habilitar.Text = "Deshabilitar";
             this.btn_habilitar.UseVisualStyleBackColor = false;
@@ -168,14 +165,15 @@
             // 
             // btn_edit
             // 
+            this.btn_edit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_edit.BackColor = System.Drawing.SystemColors.Highlight;
             this.btn_edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_edit.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_edit.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_edit.Location = new System.Drawing.Point(1446, 198);
+            this.btn_edit.Location = new System.Drawing.Point(964, 129);
             this.btn_edit.Margin = new System.Windows.Forms.Padding(0);
             this.btn_edit.Name = "btn_edit";
-            this.btn_edit.Size = new System.Drawing.Size(194, 69);
+            this.btn_edit.Size = new System.Drawing.Size(129, 45);
             this.btn_edit.TabIndex = 61;
             this.btn_edit.Text = "Editar";
             this.btn_edit.UseVisualStyleBackColor = false;
@@ -183,14 +181,15 @@
             // 
             // btn_crear
             // 
+            this.btn_crear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_crear.BackColor = System.Drawing.SystemColors.Highlight;
             this.btn_crear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_crear.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_crear.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_crear.Location = new System.Drawing.Point(1446, 357);
+            this.btn_crear.Location = new System.Drawing.Point(964, 232);
             this.btn_crear.Margin = new System.Windows.Forms.Padding(0);
             this.btn_crear.Name = "btn_crear";
-            this.btn_crear.Size = new System.Drawing.Size(194, 69);
+            this.btn_crear.Size = new System.Drawing.Size(129, 45);
             this.btn_crear.TabIndex = 62;
             this.btn_crear.Text = "Cargar Nuevo";
             this.btn_crear.UseVisualStyleBackColor = false;
@@ -198,14 +197,15 @@
             // 
             // btn_comisionar
             // 
+            this.btn_comisionar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_comisionar.BackColor = System.Drawing.SystemColors.Highlight;
             this.btn_comisionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_comisionar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_comisionar.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_comisionar.Location = new System.Drawing.Point(1446, 440);
+            this.btn_comisionar.Location = new System.Drawing.Point(964, 286);
             this.btn_comisionar.Margin = new System.Windows.Forms.Padding(0);
             this.btn_comisionar.Name = "btn_comisionar";
-            this.btn_comisionar.Size = new System.Drawing.Size(194, 69);
+            this.btn_comisionar.Size = new System.Drawing.Size(129, 45);
             this.btn_comisionar.TabIndex = 63;
             this.btn_comisionar.Text = "Rendir comisiones";
             this.btn_comisionar.UseVisualStyleBackColor = false;
@@ -216,30 +216,35 @@
             this.dataGridEmpresas.AllowUserToDeleteRows = false;
             this.dataGridEmpresas.AllowUserToOrderColumns = true;
             this.dataGridEmpresas.AllowUserToResizeRows = false;
+            this.dataGridEmpresas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridEmpresas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridEmpresas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridEmpresas.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.dataGridEmpresas.Location = new System.Drawing.Point(368, 128);
+            this.dataGridEmpresas.Location = new System.Drawing.Point(245, 83);
+            this.dataGridEmpresas.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridEmpresas.MultiSelect = false;
             this.dataGridEmpresas.Name = "dataGridEmpresas";
             this.dataGridEmpresas.ReadOnly = true;
             this.dataGridEmpresas.RowHeadersVisible = false;
             this.dataGridEmpresas.RowTemplate.Height = 28;
             this.dataGridEmpresas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridEmpresas.Size = new System.Drawing.Size(1041, 472);
+            this.dataGridEmpresas.Size = new System.Drawing.Size(694, 307);
             this.dataGridEmpresas.TabIndex = 64;
             this.dataGridEmpresas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridEmpresas_CellContentClick);
             // 
             // btn_eliminar
             // 
+            this.btn_eliminar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_eliminar.BackColor = System.Drawing.SystemColors.Highlight;
             this.btn_eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_eliminar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_eliminar.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_eliminar.Location = new System.Drawing.Point(1446, 277);
+            this.btn_eliminar.Location = new System.Drawing.Point(964, 180);
             this.btn_eliminar.Margin = new System.Windows.Forms.Padding(0);
             this.btn_eliminar.Name = "btn_eliminar";
-            this.btn_eliminar.Size = new System.Drawing.Size(194, 69);
+            this.btn_eliminar.Size = new System.Drawing.Size(129, 45);
             this.btn_eliminar.TabIndex = 65;
             this.btn_eliminar.Text = "Eliminar";
             this.btn_eliminar.UseVisualStyleBackColor = false;
@@ -248,16 +253,16 @@
             // 
             // Empresa_Listado
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1801, 711);
+            this.ClientSize = new System.Drawing.Size(1201, 462);
             this.Controls.Add(this.btn_eliminar);
             this.Controls.Add(this.dataGridEmpresas);
             this.Controls.Add(this.btn_comisionar);
             this.Controls.Add(this.btn_crear);
             this.Controls.Add(this.btn_edit);
             this.Controls.Add(this.btn_habilitar);
-            this.Margin = new System.Windows.Forms.Padding(9, 12, 9, 12);
+            this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.Name = "Empresa_Listado";
             this.Load += new System.EventHandler(this.Empresa_Listado_Load);
             this.Controls.SetChildIndex(this.footer, 0);
